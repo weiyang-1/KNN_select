75d7963c333ba7cd06eab053ee06edbaa9106770
weiyang-1 <2815054903@qq.com>
2020-10-12 16:39:21 +0800

    v1.0 add train mode judge

--- knn_select.py
+++ knn_select.py
@@ -7,6 +7,8 @@
 """
 
 
+import os
+import time
 import pandas as pd
 import numpy as np
 import pickle
@@ -14,7 +16,7 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def KNN_S():
+def knn_train():
     """
     通过A品牌与B品牌映射关系，训练出A--->B的唯一对应关系
     :param x:
@@ -39,9 +41,7 @@ def KNN_S():
         # 训练模型
         model = KNeighborsClassifier(n_neighbors=n_b)
         model.fit(train_x, train_y)
-        print(model.predict(test_x))
         score = model.score(test_x, test_y)
-        print(score)
         score_list.append(score)
 
     # 绘制参数和结果图
@@ -51,31 +51,41 @@ def KNN_S():
     # 选择最大的准确率对应的参数训练模型并且保存
     max_score_index_list = [i for i, j in enumerate(score_list) if j == max(score_list)]
     suitable_params = n_neighbors_set_list[max_score_index_list[1]] if len(max_score_index_list)>1 else n_neighbors_set_list[max_score_index_list[0]]
-    print(f"最合适的参数为： {suitable_params}")
 
     # 以最适合的参数训练并保存模型
     model = KNeighborsClassifier(n_neighbors=suitable_params)
     model.fit(train_x, train_y)
     with open("./knn_select.model", "wb") as f:
          pickle.dump(model, f)
-    print(f"保存模型成功！")
+    print(f"save model success！")
 
 
-def pre_data():
+def knn_predict():
     """
     预测数据
     :return:
     """
+    # 没有的话先训练，再预测
+    if not os.path.exists("./knn_select.model"):
+        print(f"no available model ！")
+        print(f"start train ...")
+        t_start = time.time()
+        knn_train()
+        t_end = time.time()
+        print(f"end training coast time is: {int((t_end-t_start)*1000)} ms")
+
+    # 开始预测数据
     with open("./knn_select.model", "rb") as f:
         model = pickle.load(f)
     data_test = pd.read_csv('./data/test_data.csv', sep=" ")
     test_x = [[i] for i in data_test.brandA]
     test_y = np.array(data_test.brandB)
     r = model.predict(test_x)
+    # 预测结果
     print(r)
+    # 预测准确性
     print(model.score(test_x, test_y))
 
 
 if __name__ == '__main__':
-    # KNN_S()
-    pre_data()
+    knn_predict()
